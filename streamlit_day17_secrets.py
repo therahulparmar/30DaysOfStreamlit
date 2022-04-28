7752634e8cd3b80dcc6296e1b3c3489876c89115
Rahul Parmar <80040049+therahulparmar@users.noreply.github.com>
2022-04-28 11:46:35 +0530

    Update streamlit_day17_secrets.py

--- streamlit_day17_secrets.py
+++ streamlit_day17_secrets.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title('st.secrets')
 
-st.write("username", st.secrets['username'])
-st.write("message", st.secrets['message'])
+st.write("name: ", st.secrets['username'])
+st.write("message: ", st.secrets['message'])
